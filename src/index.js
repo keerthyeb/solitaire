da47a3ac973ace4a5a3afacd77f4b0470ddc9702
Keerthyb <keerthyb@IN-Keerthyb.local>
2019-04-04 12:27:48 +0530

    Modify the UI

--- src/index.js
+++ src/index.js
@@ -11,51 +11,14 @@ class RenderCards extends React.Component {
       black: "black",
       red: "red"
     };
-    this.cards = null;
+    this.BACK = ["\uD83C\uDCA0"];
+    this.cards = lodash.shuffle(cards);
+    this.cardCount = 0;
     this.rendCards = this.rendCards.bind(this);
     this.allowDrop = this.allowDrop.bind(this);
     this.drag = this.drag.bind(this);
     this.drop = this.drop.bind(this);
-    this.generateTemplate = this.generateTemplate.bind(this);
-  }
-
-  generateTemplate() {
-    return (
-      <div className="solitaire">
-        <div className="upperContainer">
-          <div className="backAndCurrentCard">
-            <div className="back" id="back" />
-            <div className="back" id="currentCard" />
-          </div>
-          <div className="reservedcardSection">
-            <div
-              className="reservedDeckCard"
-              id="reservedCards1"
-              onDrop={this.drop}
-              onDragOver={this.allowDrop}
-            />
-            <div
-              className="reservedDeckCard"
-              id="reservedCards2"
-              onDrop={this.drop}
-              onDragOver={this.allowDrop}
-            />
-            <div
-              className="reservedDeckCard"
-              id="reservedCards3"
-              onDrop={this.drop}
-              onDragOver={this.allowDrop}
-            />
-            <div
-              className="reservedDeckCard"
-              id="reservedCards4"
-              onDrop={this.drop}
-              onDragOver={this.allowDrop}
-            />
-          </div>
-        </div>
-      </div>
-    );
+    this.rendNextCard = this.rendNextCard.bind(this);
   }
 
   allowDrop(ev) {
@@ -73,28 +36,48 @@ class RenderCards extends React.Component {
   }
 
   rendCards() {
-    let cardCount = 0;
     const cardsDiv = [];
-    this.cards = lodash.shuffle(cards);
-    for (let i = 1; i <= 7; i++) {
+    for (let i = 0; i < 7; i++) {
       const elements = [];
-      for (let j = 0; j < i; j++) {
-        let card = this.cards[cardCount];
-        elements.push(
+      for (let j = 0; j <= i; j++) {
+        let card = this.cards[this.cardCount];
+        let element = (
           <div
             id={card.color + " _" + card.number + "_" + card.type}
             className={this.cardColor[card.color]}
-            draggable
-            onDragStart={this.drag}
           >
             {card.unicode}
           </div>
         );
-        cardCount++;
+        if (j == i) {
+          element = (
+            <div
+              id={card.color + " _" + card.number + "_" + card.type}
+              className={this.cardColor[card.color]}
+              draggable
+              onDragStart={this.drag}
+            >
+              {card.unicode}
+            </div>
+          );
+        }
+
+        elements.push(element);
+        this.cardCount++;
       }
       cardsDiv.push(<div className="pile">{elements}</div>);
     }
     return <div className="piles">{cardsDiv}</div>;
+  }
+
+  rendNextCard() {
+    this.cardCount++;
+    if (this.cardCount == 52) this.cardCount = 28;
+    let card = this.cards[this.cardCount];
+    let currentCardDiv = document.getElementById("currentCard");
+    currentCardDiv.style.color = card.color;
+    currentCardDiv.innerHTML = card.unicode;
+    currentCardDiv.draggable = true;
   }
 
   render() {
@@ -102,7 +85,9 @@ class RenderCards extends React.Component {
       <div className="solitaire">
         <div className="upperContainer">
           <div className="backAndCurrentCard">
-            <div className="back" id="back" />
+            <div className="back" id="back" onClick={this.rendNextCard}>
+              {this.BACK}
+            </div>
             <div className="back" id="currentCard" />
           </div>
           <div className="reservedcardSection">
