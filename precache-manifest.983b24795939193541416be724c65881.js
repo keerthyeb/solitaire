54560237a22d1d03061baa1fc36e91ffd8045e34
Keerthyb <keerthyb@IN-Keerthyb.Dlink>
2019-04-12 22:59:47 +0530

    Updates

---
+++ precache-manifest.983b24795939193541416be724c65881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ea6d185d6444c626fef",
+    "url": "/solitaire/static/css/main.9ebd0282.chunk.css"
+  },
+  {
+    "revision": "7ea6d185d6444c626fef",
+    "url": "/solitaire/static/js/main.29de6353.chunk.js"
+  },
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "ee0cc48f05d53a4551da",
+    "url": "/solitaire/static/js/2.e348e362.chunk.js"
+  },
+  {
+    "revision": "403677eacddc5b63dc6aa8326f5891c9",
+    "url": "/solitaire/index.html"
+  }
+];
